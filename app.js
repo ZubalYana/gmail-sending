94234f0d2261a76f95dc2923bdd3fef9a9c46f4e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-03-30 20:15:10 +0200

    write the users in the text file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
-
+const fs = require('fs')
 
 const TOKEN = '7121504275:AAHPive5eXJbB8RssVnYWZeBgloZZ7nXGvs';
 const PORT = 3000;
@@ -13,9 +13,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, './')))
 app.post('/send', (req, res)=>{
-    const { name, gmail, time, id } = req.body;
+    const { name, gmail } = req.body;
     console.log(name);
     console.log(gmail);
+    fs.writeFile('users.txt', JSON.stringify(req.body), (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(`The user is saved`)
+        }
+    })
 })
 
 
